32783d08c21fc4277973f732d94cabae9d035f8a
Piotr Wezyk <piotr.wezykk@gmail.com>
2022-11-14 18:33:49 +0100

    Implement Item Model

---
+++ lib/models/item_model.dart
@@ -0,0 +1,13 @@
+class ItemModel {
+  final String id;
+  final String title;
+  final String imageURL;
+  final DateTime releaseDate;
+
+  ItemModel({
+    required this.id,
+    required this.title,
+    required this.imageURL,
+    required this.releaseDate,
+  });
+}
